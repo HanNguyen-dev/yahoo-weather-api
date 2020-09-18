41bacbcdfd5946fe558616778c5f01fcdde3fc14
Han Nguyen <han.nguyen@outlook.com>
2020-09-17 20:36:44 -0500

    update client for service

--- yahoo-weather-client/src/App.js
+++ yahoo-weather-client/src/App.js
@@ -2,58 +2,19 @@ import React, { Component } from 'react';
 import City from './city/city';
 import Weather from './weather/weather'
 import axios from 'axios';
-import { OAuth } from 'oauth';
 import './App.css';
 
 class App extends Component {
-    baseURL = 'https://weather-ydn-yql.media.yahoo.com/forecastrss';
-
     constructor(prop) {
         super();
         this.state = {};
     }
 
-    usingOAuth(city) {
-        const APPID = process.env.REACT_APP_APPID;
-        const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-        const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
-        console.log(city);
-
-        const URL = this.baseURL + '?location=' + city + '&format=json';
-        let header = { "X-Yahoo-App-Id": APPID };
-        let request = new OAuth(
-            null,
-            null,
-            CLIENT_ID,
-            CLIENT_SECRET,
-            '1.0',
-            null,
-            'HMAC-SHA1',
-            null,
-            header
-        );
-
-        request.get(
-            URL,
-            null,
-            null,
-            (err, data, result) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    this.setState( { forecast: JSON.parse(data) });
-                    console.log(data);
-                }
-            }
-        );
-
-    }
-    requestAPI(city) {
-        const URL = this.baseURL + '?city=' + encodeURIComponent(city);
+    getForecasts(city) {
+        const URL = process.env.REACT_APP_BASE_URL + '?city=' + encodeURIComponent(city);
 
         axios.get(URL).then(
                 (response) => {
-                    console.log(response);
                     this.setState( { forecast: response.data })
                 })
             .catch(
@@ -64,7 +25,7 @@ class App extends Component {
 
     updateCity(city) {
         this.setState( { city: city } );
-        this.usingOAuth(city);
+        this.getForecasts(city);
     }
 
     render() {
--- yahoo-weather-client/src/weather/forecast/day/day.js
+++ yahoo-weather-client/src/weather/forecast/day/day.js
@@ -4,7 +4,6 @@ import './day.css';
 class Day extends Component {
 
     render() {
-
         return (
             <div class="day">
                 <b>{ this.props.day }</b> <br/>
